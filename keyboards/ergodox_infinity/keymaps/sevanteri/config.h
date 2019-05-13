5429901f9ad1e203912e8bbe4e70c3d071e55e69
Pete Sevander <pete@sevander.com>
2019-05-13 12:22:59 +0300

    fix fuckups and add ESCCTRL

---
+++ keyboards/ergodox_infinity/keymaps/sevanteri/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+
+#include "../../config.h"
+
+//#define TAPPING_TOGGLE 3
+
+#define PERMISSIVE_HOLD
+// #define RETRO_TAPPING
+
+
+
+#endif
--- keyboards/ergodox_infinity/keymaps/sevanteri/keymap.c
+++ keyboards/ergodox_infinity/keymaps/sevanteri/keymap.c
@@ -12,6 +12,9 @@
 #define FUNCL 1 // function layer
 #define NUMP 2 // numpad layer
 
+// keys
+#define ESCCTRL MT(MOD_LCTL, KC_ESC) // hold esc for ctrl
+
 enum custom_keycodes {
     PLACEHOLDER = SAFE_RANGE, // can always be here
     EPRM,
@@ -48,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_NUBS,  KC_1,    KC_2,   KC_3,    KC_4, KC_5, TG(NUMP),
         KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R, KC_T, KC_RBRC,
-        KC_CAPS,  KC_A,    KC_S,   KC_D,    KC_F, KC_G,
+        ESCCTRL,   KC_A,    KC_S,   KC_D,    KC_F, KC_G,
         KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V, KC_B, KC_DELT,
         KC_LCTRL, KC_BSLS, KC_GRV, KC_BTN2, KC_BTN1,
 
@@ -58,14 +61,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         // right hand
         TG(NUMP), KC_6, KC_7,    KC_8,    KC_9,   KC_0,     KC_MINS,
-        KC_LBRC,  KC_Y, KC_U,    KC_I,    KC_O,   KC_P,     KC_LBRC,
+        KC_LBRC,  KC_Y, KC_U,    KC_I,    KC_O,   KC_P,     KC_NUHS,
         /**/      KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
         KC_BSPC,  KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
         /**/      /**/  KC_RALT, KC_PSCR, KC_INS, KC_RCTRL, KC_RGUI,
 
         KC_PGUP,   KC_RCTRL,
         KC_PGDN,
-        MO(FUNCL), KC_ENT, KC_BSPC
+        MO(FUNCL), KC_ENT, KC_SPC
     ),
 
 /* Keymap 1: Basic layer with functions
