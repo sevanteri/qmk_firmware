e644d09d961b672e712819c74b44e0b1d9c008df
Pete Sevander <pete@sevander.com>
2019-05-13 10:43:46 +0300

    my layout from kiibohd firmware

--- keyboards/ergodox_infinity/keymaps/sevanteri/keymap.c
+++ keyboards/ergodox_infinity/keymaps/sevanteri/keymap.c
@@ -10,7 +10,7 @@
 // Layer names
 #define BASE 0 // default layer
 #define FUNCL 1 // function layer
-#define SYMB 2 // symbol layer
+#define NUMP 2 // numpad layer
 #define MDIA 3 // media keys
 
 enum custom_keycodes {
@@ -23,135 +23,147 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
- * 
+ *
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  Esc   |   1  |   2  |   3  |   4  |   5  |  §½  |           | PRSC |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |  ISO/  |   1  |   2  |   3  |   4  |   5  | NUMP |           | NUMP |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   Å    |
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  |  ¨^  |           |  Å   |   Y  |   U  |   I  |   O  |   P  |   '    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  Caps  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   Ö  |   Ä    |
- * |--------+------+------+------+------+------|  L2  |           |  L2  |------+------+------+------+------+--------|
+ * |  ESC   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   Ö  |   Ä    |
+ * |--------+------+------+------+------+------| DEL  |           | BSPC |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  -   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Ctrl |  ¨^  |  <|>  | Alt | LGui|                                       | Ctrl |  Alt  |  ´`  |   +  | RGui |
+ *   | Ctrl |  ´`  |  §½  | RBTN | LBTN |                                       | RALT | PRSC | INS  |RCTRL | RGui |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | LEFT |RIGHT |       | DOWN | UP   |
+ *                                        | LEFT |RIGHT |       | PgUp |RCTRL |
  *                                 ,------+------+------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Del   |------|       |------|  Enter | Bkspc|
- *                                 |      |      | End  |       | PgDn |        |      |
+ *                                 |      |      | Home |       | PgDn |        |      |
+ *                                 | LALT | LGUI |------|       |------|  Enter | Space|
+ *                                 |      |      | End  |       |FUNCL |        |      |
  *                                 `--------------------'       `----------------------'
  */
 
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,    KC_1,  KC_2,  KC_3,  KC_4,  KC_5,   KC_GRV,
-        KC_TAB,    KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,   TG(1),
-        KC_CAPS,   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,
-        KC_LSFT,   KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,   MO(2),
-        KC_LCTRL,  KC_RBRC, KC_NONUS_BSLASH,  KC_LALT,  KC_LGUI,
-                                               KC_LEFT, KC_RIGHT,
-                                                              KC_HOME,
-                                               KC_SPC,KC_DELT,KC_END,
+        KC_NUBS,  KC_1,    KC_2,   KC_3,    KC_4, KC_5, TG(NUMP),
+        KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R, KC_T, KC_RBRC,
+        KC_CAPS,  KC_A,    KC_S,   KC_D,    KC_F, KC_G,
+        KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V, KC_B, KC_DELT,
+        KC_LCTRL, KC_BSLS, KC_GRV, KC_BTN2, KC_BTN1,
+
+                                          /**/     KC_HOME, KC_END,
+                                          /**/     /**/     MO(FUNCL),
+                                          KC_LALT, KC_LGUI, KC_LCTRL,
+
         // right hand
-        KC_PSCREEN,  KC_6,   KC_7,    KC_8,    KC_9,   KC_0,        KC_MINS,
-        TG(1),   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,        KC_LBRC,
-                     KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN,     KC_QUOT,
-        MO(2),    KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,     KC_RSFT,
-                             KC_RCTRL, KC_RALT,   KC_BSLS,  KC_EQL, KC_RGUI,
-        KC_DOWN, KC_UP,
-        KC_PGUP,
-        KC_PGDN, KC_ENT, KC_BSPC
+        TG(NUMP), KC_6, KC_7,    KC_8,    KC_9,   KC_0,     KC_MINS,
+        KC_LBRC,  KC_Y, KC_U,    KC_I,    KC_O,   KC_P,     KC_LBRC,
+        /**/      KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
+        KC_BSPC,  KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
+        /**/      /**/  KC_RALT, KC_PSCR, KC_INS, KC_RCTRL, KC_RGUI,
+
+        KC_PGUP,   KC_RCTRL,
+        KC_PGDN,
+        MO(FUNCL), KC_ENT, KC_BSPC
     ),
 
 /* Keymap 1: Basic layer with functions
  *
- * 
+ *
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  Esc   |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |           | F12 |  F6  |  F7  |  F8  |  F9  |   0  |   -    |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |           | F12  |  F6  |  F7  |  F8  |  F9  | F10  |  F11   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   Å    |
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  Caps  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   Ö  |   Ä    |
- * |--------+------+------+------+------+------|  L2  |           |  L2  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  -   | RShift |
+ * |        |      |      |      |      |      |------|           |------|      | LEFT | DOWN |  UP  |RIGHT |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Ctrl |  ¨^  |  <|>  | Alt | LGui|                                       | Ctrl |  Alt  |  ´`  |   +  | RGui |
+ *   |      |      |       |     |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | LEFT |RIGHT |       | DOWN | UP   |
+ *                                        |      |BLTOG |       |      |      |
  *                                 ,------+------+------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Del   |------|       |------|  Enter | Bkspc|
- *                                 |      |      | End  |       | PgDn |        |      |
+ *                                 |      |      | BL+  |       |      |        |      |
+ *                                 |      |      |------|       |------|        |      |
+ *                                 |      |      | BL-  |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
 
 [FUNCL] = LAYOUT_ergodox(  // layer 1 : functions
+
   // left hand
   _______,  KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F11,
   _______,  _______,  _______,  _______,  _______,  _______,  _______,
   _______,  _______,  _______,  _______,  _______,  _______,
   _______,  _______,  _______,  _______,  _______,  _______,  _______,
   _______,  _______,  _______,  _______,  _______,
-                                         _______, _______,
-                                                        _______,
-                                         _______,_______,_______,
+
+                                         /**/     _______, BL_TOGG,
+                                         /**/     /**/     BL_INC,
+                                         _______, _______, BL_DEC,
+
   // right hand
-  KC_F12,  KC_F6,   KC_F7,    KC_F8,    KC_F9,   KC_F10,     _______,
-  _______,    _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______,
-  _______,    _______,  _______,  _______,  _______,  _______,  _______,
-                      _______,  _______,  _______,  _______,  _______,
+  KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  _______,
+  _______, _______, _______, _______, _______,  _______, _______,
+  /**/     KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
+  _______, _______, _______, _______, _______,  _______, _______,
+  /**/     /**/     _______, _______, _______,  _______, _______,
+
   _______, _______,
   _______,
   _______, _______, _______
 ),
 
-/* Keymap 2: Symbol Layer
+/* Keymap 2: Numpad Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+ * |        |      |      |      |      |      |      |           |      |      |NUMLCK|  P/  |  P*  |  P-  |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+ * |        |      |      |      |      |      |      |           |      |      |   7  |   8  |   9  |  P+  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
+ * |        |      |      |      |      |      |------|           |------|      |   4  |   5  |   6  |  P+  |        |
+ * |--------+------+------+------+------+------|      |           | BSPC |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |   1  |   2  |   3  |Penter|        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |    . |   0  |   =  |      |
+ *   |      |      |       |     |      |                                       |      |      |  P.  |Penter|      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |Animat| BLTOG|       |Toggle|Solid |
+ *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |Bright|Bright|  BL+ |       |      |Hue-  |Hue+  |
- *                                 |ness- |ness+ |------|       |------|      |      |
- *                                 |      |      |  BL- |       |      |      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|Enter |  P0  |
+ *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-// SYMBOLS
-[SYMB] = LAYOUT_ergodox(
-       // left hand
-       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,DE_EXLM,DE_AT,  DE_LCBR,DE_RCBR,DE_PIPE,KC_TRNS,
-       KC_TRNS,DE_HASH,DE_DLR, DE_LPRN,DE_RPRN,DE_GRV,
-       KC_TRNS,DE_PERC,DE_CIRC,DE_LBRC,DE_RBRC,DE_TILD,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       RGB_MOD,BL_TOGG,
-                                               BL_INC,
-                               RGB_VAD,RGB_VAI,BL_DEC,
-       // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    DE_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    DE_PLUS, KC_TRNS,
-       KC_TRNS, DE_AMPR, KC_1,   KC_2,    KC_3,    DE_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    DE_EQL,  KC_TRNS,
-       RGB_TOG, RGB_SLD,
-       KC_TRNS,
-       KC_TRNS, RGB_HUD, RGB_HUI
+// NUMPAD
+[NUMP] = LAYOUT_ergodox(
+
+// left hand
+    _______,  KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F11,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,
+
+                                             /**/     _______, _______,
+                                             /**/     /**/     _______,
+                                             _______, _______, _______,
+
+// right hand
+   _______, _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMSN, _______,
+   _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______,
+   /**/     _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,
+   KC_BSPC, _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______,
+   /**/     /**/     _______, _______, KC_PDOT, KC_PENT, _______,
+
+   _______, _______,
+   _______,
+   _______, KC_ENT,  KC_P0
+
 ),
 
 /* Keymap 3: Media and mouse keys
@@ -240,7 +252,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
   }
   return true;
-}  
+}
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
@@ -249,21 +261,21 @@ void matrix_init_user(void) {
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
-    
+
     uint8_t layer = biton32(layer_state);
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
-    ergodox_right_led_3_off();
+    /* ergodox_right_led_3_off(); */
     switch (layer) {
-        case 1:
+        case FUNCL:
             ergodox_right_led_1_on();
             break;
-        case 2:
+        case NUMP:
             ergodox_right_led_2_on();
             break;
         default:
             // none
             break;
-    }   
+    }
 };
