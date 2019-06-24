b11ad12fc4737dc6b805393afbc9fa3c599bb1ec
Pete Sevander <pete@sevander.com>
2019-06-24 10:34:44 +0300

    swap hands buttons

--- keyboards/ergodox_infinity/keymaps/sevanteri/keymap.c
+++ keyboards/ergodox_infinity/keymaps/sevanteri/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,    KC_RBRC,   /*****/ KC_LBRC,   KC_Y,    KC_U,   KC_I,     KC_O,   KC_P,    KC_NUHS,
     CTRLESC,  KC_A,   KC_S,   KC_D,    KC_F,    KC_G,    /**/       /*****/ /**/       KC_H,    KC_J,   KC_K,     KC_L,   KC_SCLN, KC_QUOT,
     LSFT_LT,  KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,    KC_DELT,   /*****/ KC_BSPC,   KC_N,    KC_M,   KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-    KC_LCTRL, KC_EQL, KC_GRV, KC_BTN2, KC_BTN1, /**/     /**/       /*****/ KC_RALT,   KC_PSCR, KC_INS, KC_RCTRL, KC_RGUI,
+    SH_MON,   KC_EQL, KC_GRV, KC_BTN2, KC_BTN1, /**/     /**/       /*****/ KC_RALT,   KC_PSCR, KC_INS, KC_RGUI,  SH_MON,
 
     /**/      /**/    /**/    /**/     /**/     KC_HOME, KC_END,    /*****/ KC_PGUP,   KC_RCTRL,
     /**/      /**/    /**/    /**/     /**/     /**/     MO(FUNCL), /*****/ KC_PGDN,
